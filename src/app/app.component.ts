733328c6570970a108e11ceefceb6c513a32ce9d
Chip <hector21096@gmail.com>
2019-06-23 23:37:37 -0500

    Utilizar el servicio GitSearch en el componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from "./git-search.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GitSearchService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private GitSearchService: GitSearchService){
+
+  }
+  ngOnInit() {
+    this.GitSearchService.gitSearch('angular').then((response) => {
+      alert("Total de librerias encontradas: " + response.total_count);
+    }, (error) => {
+      alert("Error: " + error.statusText);
+    })
+  }
+  
   title = 'Github Browser';
 }
